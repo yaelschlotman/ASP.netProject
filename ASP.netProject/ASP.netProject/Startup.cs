8fd18b60b7e7edfe933ee099dbb1f7c770c6cdad
yaelschlotman <koshercouponmom@gmail.com>
2016-03-22 09:08:45 -0500

    structure

---
+++ ASP.netProject/ASP.netProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.netProject.Startup))]
+namespace ASP.netProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
